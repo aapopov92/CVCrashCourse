4ad3aee00c107c2fb0fded89119e81413e9d7acb
Oleksii Popov <aapopov@i.ua>
2020-01-19 23:23:40 +0200

    Finished day 5 and day 6

--- day2/opencv_tutorial_01.py
+++ day2/opencv_tutorial_01.py
@@ -10,82 +10,82 @@ image = cv2.imread("jp.png")
 h, w, d = image.shape
 print(f"Width={w}, height={h}, depth={d}")
 
-# # display the image to our screen -- we will need to click the window
-# # open by OpenCV and press a key on our keyboard to continue execution
-# cv2.imshow("Tratata", image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+# display the image to our screen -- we will need to click the window
+# open by OpenCV and press a key on our keyboard to continue execution
+cv2.imshow("Tratata", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-# # access the RGB pixel located at x=50, y=100, keepind in mind that
-# # OpenCV stores images in BGR order rather than RGB
-# B, G, R = image[100, 50]
-# print(f"R={R}, G={G}, B={B}")
+# access the RGB pixel located at x=50, y=100, keepind in mind that
+# OpenCV stores images in BGR order rather than RGB
+B, G, R = image[100, 50]
+print(f"R={R}, G={G}, B={B}")
 
-# # extract a 100x100 pixel square ROI (Region of Interest) from the
-# # input image starting at x=320,y=60 at ending at x=420,y=160
-# ROI = image[60:160, 320:420]
-# cv2.imshow("ROI", ROI)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+# extract a 100x100 pixel square ROI (Region of Interest) from the
+# input image starting at x=320,y=60 at ending at x=420,y=160
+ROI = image[60:160, 320:420]
+cv2.imshow("ROI", ROI)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-# # resize the image to 200x200px, ignoring aspect ratio
-# resized = cv2.resize(image, (200, 200))
-# cv2.imshow("Resized", resized)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+# resize the image to 200x200px, ignoring aspect ratio
+resized = cv2.resize(image, (200, 200))
+cv2.imshow("Resized", resized)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-# # fixed resizing and distort aspect ratio so let's resize the width
-# # to be 300px but compute the new height based on the aspect ratio
-# r = 300.0 / w
-# dimesions = (300, int(h * r))
-# resized = cv2.resize(image, dimesions)
-# cv2.imshow("Resized with aspect ratio", resized)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+# fixed resizing and distort aspect ratio so let's resize the width
+# to be 300px but compute the new height based on the aspect ratio
+r = 300.0 / w
+dimesions = (300, int(h * r))
+resized = cv2.resize(image, dimesions)
+cv2.imshow("Resized with aspect ratio", resized)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-# # manually computing the aspect ratio can be a pain so let's use the
-# # imutils library instead
-# resized = imutils.resize(image, width=300)
-# cv2.imshow("Resized by imutils", resized)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+# manually computing the aspect ratio can be a pain so let's use the
+# imutils library instead
+resized = imutils.resize(image, width=300)
+cv2.imshow("Resized by imutils", resized)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-# # let's rotate an image 45 degrees clockwise using OpenCV by first
-# # computing the image center, then constructing the rotation matrix,
-# # and then finally applying the affine warp
-# center = w // 2, h // 2
-# M = cv2.getRotationMatrix2D(center, -45, 1.0)
-# rotated = cv2.warpAffine(image, M, (w, h))
-# cv2.imshow("Rotated", rotated)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+# let's rotate an image 45 degrees clockwise using OpenCV by first
+# computing the image center, then constructing the rotation matrix,
+# and then finally applying the affine warp
+center = w // 2, h // 2
+M = cv2.getRotationMatrix2D(center, -45, 1.0)
+rotated = cv2.warpAffine(image, M, (w, h))
+cv2.imshow("Rotated", rotated)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-# # rotation can also be easily accomplished via imutils with less code
-# rotated = imutils.rotate(image, 45)
-# cv2.imshow("Rotated by imutils", rotated)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+# rotation can also be easily accomplished via imutils with less code
+rotated = imutils.rotate(image, 45)
+cv2.imshow("Rotated by imutils", rotated)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-# # OpenCV doesn't "care" if our rotated image is clipped after rotation
-# # so we can instead use another imutils convenience function to help
-# # us out
-# rotated = imutils.rotate_bound(image, 45)
-# cv2.imshow("Rotated by imutils", rotated)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+# OpenCV doesn't "care" if our rotated image is clipped after rotation
+# so we can instead use another imutils convenience function to help
+# us out
+rotated = imutils.rotate_bound(image, 45)
+cv2.imshow("Rotated by imutils", rotated)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-# for angle in np.arange(0, 360, 4):
-# 	rotated = imutils.rotate(image, angle)
-# 	cv2.imshow("Rotated (Problematic)", rotated)
-# 	if cv2.waitKey(1) & 0xFF == ord('q'):
-# 		break
+for angle in np.arange(0, 360, 4):
+	rotated = imutils.rotate(image, angle)
+	cv2.imshow("Rotated (Problematic)", rotated)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
 
-# # apply a Gaussian blur with a 11x11 kernel to the image to smooth it,
-# # useful when reducing high frequency noise
-# blur = cv2.GaussianBlur(image, (11,11), 0)
-# cv2.imshow("Gaussian Blur", blur)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+# apply a Gaussian blur with a 11x11 kernel to the image to smooth it,
+# useful when reducing high frequency noise
+blur = cv2.GaussianBlur(image, (11,11), 0)
+cv2.imshow("Gaussian Blur", blur)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 # draw a 2px thick red rectangle surrounding the face
 output = image.copy()
@@ -116,6 +116,8 @@ cv2.putText(output, "OpenCV + Jurassic Park!!!", (300, 150),
 cv2.imshow("Text", output)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
 
 
 
